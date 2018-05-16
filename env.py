26d7d1ebef37ca9504da824c4013c5847539855a
wzkDTerH <694994343@qq.com>
2018-05-16 23:02:25 +0800

    done...NKmodel

--- env.py
+++ env.py
@@ -1,36 +1,76 @@
 from config import all_config
 import moniter
 import logging
+import random
 from copy import deepcopy
 
-class Topomap:
-    def __init__(self, config):
-        self.N = config["N"]
-        self.K = config["K"]
-        self.minh = config["minh"]
-        self.maxh = config["maxh"]
-        self.M = config["M"]
-        self.seed = None
 
-    def gen_seed(self):
-        pass
+class NKmodel:
+    def __init__(self, n, k):
+        assert (type(n) == int and type(k) == int)
+        assert (0 <= k < n)
+        logging.debug("Init NK model n=%d,k=%d" % (n, k))
+        self.N = n
+        self.K = k
+        self.theta_func = self.genRandTheta(self.N, self.K)
 
-    def get_seed(self):
-        if(self.seed is None):
-        logging.error("Gen seed first")
-        raise Exception("no seed")
+    def genRandTheta(self, n, k):
+        logging.debug("Init NK model -> genRandTheta")
+        k_c_max = 2 ** (k + 1)
+        logging.debug("genRandTheta: k_c_max %d*%d=%d" % (n, k_c_max, n * k_c_max))
+        ret_map = []
+        for i in range(n):
+            ret_map.append([])
+            for j in range(k_c_max):
+                ret_map[i].append(random.random())
+        return ret_map
 
-    def set_seed(self, seed):
-        self.seed = deepcopy(seed)
+    def getGrayCode(self, no):
+        ret_code = []
+        for i in range(self.N):
+            ret_code.append(no % 2)
+            no /= 2
+        for i in range(self.N - 1):
+            ret_code[i] = ret_code[i + 1] ^ ret_code[i]
+        return ret_code
 
-    def genMap(self):
-        pass
+    def getValueFromStates(self, code):
+        assert (len(code) == self.N)
+
+        def code2int(t_code):
+            return sum([t_code[i] * (2 ** int(i)) for i in range(len(t_code))])
+
+        rtn_value = 0
+        code_t = code + code
+        for i in range(self.N):
+            rtn_value += self.theta_func[i][code2int(code_t[i:i + self.K + 1])]
+        return rtn_value / self.N
+
+    def getValue(self, x):
+        assert (type(x) == int)
+        assert (0 <= x < 2 ** self.N)
+        rtn_value = self.getValueFromStates(self.getGrayCode(x))
+        logging.debug("nkmodel->getValue {} rtn={}".format(x, rtn_value))
+        return rtn_value
+
 
 class Env:
-    def __init__(self):
-        pass
+    def __init__(self, N, K, T):
+        self.models = {"st": NKmodel(N,K),
+                       "ed": NKmodel(N,K)}
+        self.T = T
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
+    import numpy as np
     all_config.load()
     moniter.LogInit()
-    tp=Topomap()
+    N = 20
+    k = 3
+    for i in range(10):
+        nkmodel = NKmodel(N, k)
+        point_pairs = []
+        for j in range(2**N):
+            point_pairs.append(nkmodel.getValue(j))
+        npa = np.array(point_pairs)
+        logging.info("N:%d, k:%d, mu=%0.4f, sigma=%0.4f"%(N,k,npa.mean(), npa.std()))
+    moniter.DrawHist(point_pairs)
--- moniter.py
+++ moniter.py
@@ -14,3 +14,73 @@ def LogInit():
     formatter = logging.Formatter('%(asctime)s %(funcName)s:%(lineno)d[%(levelname)s] %(message)s')
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
+
+def Draw3dPairs(point_pairs):
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    import numpy as np
+
+    x = np.array(point_pairs)[:,0]
+    y = np.array(point_pairs)[:,1]
+    z = np.array(point_pairs)[:,2]
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True, cmap=cm.gist_earth,)
+
+    plt.show()
+
+def DrawHist(nums, label=None):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib import mlab
+
+    x = np.array(nums)
+    n_bins = int(np.sqrt(len(x) * 2))
+    mu = x.mean()
+    sigma = x.std()
+    logging.info("len(x)= %d, n_bins= %d, mu= %0.3f, sigma= %0.3f"%(len(x), n_bins, mu, sigma))
+    fig, ax = plt.subplots(figsize=(8, 4))
+
+    # plot the cumulative histogram
+    n, bins, patches = ax.hist(x, n_bins, normed=1, histtype='stepfilled', label=label, alpha=0.7)
+
+    # Add a line showing the expected distribution.
+    y = mlab.normpdf(bins, mu, sigma)
+    ax.plot(bins, y, 'k--', linewidth=1.5)
+
+    # tidy up the figure
+    ax.grid(True)
+    ax.legend(loc='right')
+    ax.set_title(r'Histogram: $\mu=%0.3f$, $\sigma=%0.3f$'%(mu, sigma))
+    plt.show()
+
+def animation_demo():
+    from matplotlib import pyplot as plt
+    from matplotlib import animation
+    import numpy as np
+    fig, ax = plt.subplots()
+
+    x = np.arange(0, 2*np.pi, 0.01)
+    line, = ax.plot(x, np.sin(x))
+
+    def animate(i):
+        line.set_ydata(np.sin(x + i / 10.0)*np.sin(i/20.0)*0.5)
+        return line,
+
+    def init():
+        line.set_ydata(np.sin(x))
+        return line,
+
+    ani = animation.FuncAnimation(fig=fig,
+                                  func=animate,
+                                  frames=1000,
+                                  init_func=init,
+                                  interval=20,
+                                  blit=False)
+
+    plt.show()
+
+if( __name__ == "__main__"):
+    animation_demo()
